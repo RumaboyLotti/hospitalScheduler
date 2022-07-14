07a467bee63e42bb7ac442486de82a2bd4574eba
AsoftLotti <AsoftLotti@outlook.com>
2022-07-14 00:19:34 -0500

    finished patient class

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,8 @@ public class Main {
 
     public static void main(String[] args) {
        Patient patrick = new Patient("patrick");
-       patrick.setAge(24);
-       patrick.setAppointment("p","patrick",11/23/2029);
+       patrick.getAge();
+       patrick.getAppointment();
+
     }
 }
--- src/Patient.java
+++ src/Patient.java
@@ -1,11 +1,14 @@
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class Patient {
 
     private String name,identifier;
-    private Integer Age, number;
+    private Integer Age;
+    private String number;
     private Date date;
     private ArrayList<String> list = new ArrayList<String>();
 
@@ -32,6 +35,10 @@ public class Patient {
 
 
     public Integer getAge() {
+        if (Age == null) {
+
+            System.out.println("Please enter your age first");
+        }
         return Age;
     }
 
@@ -39,11 +46,19 @@ public class Patient {
         Age = age;
     }
 
-    public Integer getNumber() {
+    public String getNumber() {
         return number;
     }
 
-    public void setNumber(Integer number) {
+    public void setNumber(String number) {
+        String allCountryRegex = "^(\\+\\d{1,3}( )?)?((\\(\\d{1,3}\\))|\\d{1,3})[- .]?\\d{3,4}[- .]?\\d{4}$";
+        Pattern p = Pattern.compile(allCountryRegex);
+        Matcher m = p.matcher(number);
+        if (m.matches()) {
+            System.out.println("Number is accepted");
+        } else {
+            System.out.println("Please Enter a number in the following format 314-111-1111");
+        }
         this.number = number;
     }
 }
